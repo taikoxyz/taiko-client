2774ae5abdc978d6f76a8f71e0899c7653365137
David <david@taikocha.in>
2022-10-11 13:30:49 +0800

    feat: update `validateAnchorTx`

--- prover/anchor.go
+++ prover/anchor.go
@@ -4,17 +4,30 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/taikochain/taiko-client/bindings/encoding"
+)
+
+// Address of the golden touch account.
+var (
+	goldenTouchAddress = common.HexToAddress("0x0000777735367b36bC9B61C50022d9D0700dB4Ec")
 )
 
 // validateAnchorTx checks whether the given transaction is a successfully
 // executed `TaikoL2.anchor` transaction.
-// TODO: verify transaction sender
 func (p *Prover) validateAnchorTx(ctx context.Context, tx *types.Transaction) error {
 	if tx.To() == nil || *tx.To() != p.cfg.TaikoL2Address {
 		return fmt.Errorf("invalid TaikoL2.anchor to: %s", tx.To())
+	}
+
+	sender, err := types.LatestSignerForChainID(p.rpc.L2ChainID).Sender(tx)
+	if err != nil {
+		return fmt.Errorf("failed to get TaikoL2.anchor transaction sender: %w", err)
+	}
+
+	if sender != goldenTouchAddress {
+		return fmt.Errorf("invalid TaikoL2.anchor transaction sender: %s", sender)
 	}
 
 	method, err := encoding.TaikoL2ABI.MethodById(tx.Data())
@@ -26,8 +39,6 @@ func (p *Prover) validateAnchorTx(ctx context.Context, tx *types.Transaction) er
 	if err != nil {
 		return fmt.Errorf("failed to get TaikoL2.anchor receipt, err: %w", err)
 	}
-
-	log.Debug("TaikoL2.anchor transaction", "height", receipt.BlockNumber, "status", receipt.Status)
 
 	if receipt.Status != types.ReceiptStatusSuccessful {
 		return fmt.Errorf("invalid TaikoL2.anchor receipt status: %d", receipt.Status)
