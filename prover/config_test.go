ee0178570d509e9adfe6e565fc9d02a17fea348f
David <david@taikocha.in>
2022-12-15 17:19:57 +0800

    test: more tests

--- prover/config_test.go
+++ prover/config_test.go
@@ -3,6 +3,7 @@ package prover
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/taikoxyz/taiko-client/cmd/flags"
@@ -23,6 +24,7 @@ func (s *ProverTestSuite) TestNewConfigFromCliContext() {
 		&cli.StringFlag{Name: flags.TaikoL2Address.Name},
 		&cli.StringFlag{Name: flags.L1ProverPrivKey.Name},
 		&cli.BoolFlag{Name: flags.Dummy.Name},
+		&cli.StringFlag{Name: flags.RandomDummyProofDelay.Name},
 	}
 	app.Action = func(ctx *cli.Context) error {
 		c, err := NewConfigFromCliContext(ctx)
@@ -35,6 +37,8 @@ func (s *ProverTestSuite) TestNewConfigFromCliContext() {
 			crypto.PubkeyToAddress(s.p.cfg.L1ProverPrivKey.PublicKey),
 			crypto.PubkeyToAddress(c.L1ProverPrivKey.PublicKey),
 		)
+		s.Equal(30*time.Minute, *c.RandomDummyProofDelayLowerBound)
+		s.Equal(time.Hour, *c.RandomDummyProofDelayUpperBound)
 		s.True(c.Dummy)
 		s.Nil(new(Prover).InitFromCli(context.Background(), ctx))
 
@@ -49,5 +53,6 @@ func (s *ProverTestSuite) TestNewConfigFromCliContext() {
 		"-" + flags.TaikoL2Address.Name, taikoL2,
 		"-" + flags.L1ProverPrivKey.Name, os.Getenv("L1_PROVER_PRIVATE_KEY"),
 		"-" + flags.Dummy.Name,
+		"-" + flags.RandomDummyProofDelay.Name, "30m-1h",
 	}))
 }
