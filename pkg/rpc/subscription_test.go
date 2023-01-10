2cc58b32196568bd6ef8e6de911b758a90023270
David <david@taiko.xyz>
2023-01-10 19:39:25 +0800

    test: update tests

---
+++ pkg/rpc/subscription_test.go
@@ -0,0 +1,52 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+	"github.com/stretchr/testify/require"
+	"github.com/taikoxyz/taiko-client/bindings"
+)
+
+func TestSubscribeEvent(t *testing.T) {
+	require.NotNil(t, SubscribeEvent("test", func(ctx context.Context) (event.Subscription, error) {
+		return event.NewSubscription(func(c <-chan struct{}) error { return nil }), nil
+	}))
+}
+
+func TestSubscribeBlockVerified(t *testing.T) {
+	require.NotNil(t, SubscribeBlockVerified(
+		newTestClient(t).TaikoL1,
+		make(chan *bindings.TaikoL1ClientBlockVerified, 1024)),
+	)
+}
+
+func TestSubscribeBlockProposed(t *testing.T) {
+	require.NotNil(t, SubscribeBlockProposed(
+		newTestClient(t).TaikoL1,
+		make(chan *bindings.TaikoL1ClientBlockProposed, 1024)),
+	)
+}
+
+func TestSubscribeSubscribeHeaderSynced(t *testing.T) {
+	require.NotNil(t, SubscribeHeaderSynced(
+		newTestClient(t).TaikoL1,
+		make(chan *bindings.TaikoL1ClientHeaderSynced, 1024)),
+	)
+}
+
+func TestSubscribeBlockProven(t *testing.T) {
+	require.NotNil(t, SubscribeBlockProven(
+		newTestClient(t).TaikoL1,
+		make(chan *bindings.TaikoL1ClientBlockProven, 1024)),
+	)
+}
+
+func TestSubscribeChainHead(t *testing.T) {
+	require.NotNil(t, SubscribeChainHead(
+		newTestClient(t).L1,
+		make(chan *types.Header, 1024)),
+	)
+}
