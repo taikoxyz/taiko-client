7304bbe80c77a5a1b6aaffbf2ffbf0a2223ac6ee
David <david@taikocha.in>
2023-02-19 14:42:47 +0800

    feat(proposer): remove `poolContentSplitter` in proposer

--- pkg/rpc/methods_test.go
+++ pkg/rpc/methods_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 )
@@ -18,13 +17,6 @@ var (
 	testAddress2 = common.HexToAddress("0x283593Cd94F70EE3ded6eF92a46Da5Aa8803e7bf")
 )
 
-func TestL2PoolContent(t *testing.T) {
-	client := newTestClient(t)
-
-	_, _, err := client.L2PoolContent(context.Background())
-	require.Nil(t, err)
-}
-
 func TestL2AccountNonce(t *testing.T) {
 	client := newTestClient(t)
 
@@ -32,63 +24,6 @@ func TestL2AccountNonce(t *testing.T) {
 
 	require.Nil(t, err)
 	require.Zero(t, nonce)
-}
-
-func TestPoolContentLen(t *testing.T) {
-	poolContent := &PoolContent{
-		testAddress1: map[string]*types.Transaction{
-			"6": types.NewTransaction(6, common.Address{}, common.Big0, 0, common.Big0, []byte{}),
-			"5": types.NewTransaction(5, common.Address{}, common.Big0, 0, common.Big0, []byte{}),
-			"7": types.NewTransaction(7, common.Address{}, common.Big0, 0, common.Big0, []byte{}),
-		},
-		testAddress2: map[string]*types.Transaction{
-			"2": types.NewTransaction(2, common.Address{}, common.Big0, 0, common.Big0, []byte{}),
-			"1": types.NewTransaction(1, common.Address{}, common.Big0, 0, common.Big0, []byte{}),
-		},
-	}
-
-	require.Equal(t, 5, poolContent.Len())
-}
-
-func TestToTxsByPriceAndNonce(t *testing.T) {
-	localPrivKey, err := crypto.ToECDSA(
-		common.Hex2Bytes("6977077fd38e2802ec39636bfed7444f1bf93e7ed55bd900a2dbd2caa3787cf5"),
-	)
-	require.Nil(t, err)
-	localAddress := common.HexToAddress("0xc4c76aFa357f95fc35c1ddE165fafaB6449DB639")
-
-	signer := types.LatestSignerForChainID(common.Big1)
-	poolContent := &PoolContent{
-		testAddress1: map[string]*types.Transaction{
-			"6": types.MustSignNewTx(prviKey1, signer, &types.LegacyTx{Gas: 21000, Nonce: 6}),
-			"5": types.MustSignNewTx(prviKey1, signer, &types.LegacyTx{Gas: 21000, Nonce: 5}),
-			"7": types.MustSignNewTx(prviKey1, signer, &types.LegacyTx{Gas: 21000, Nonce: 7}),
-		},
-		testAddress2: map[string]*types.Transaction{
-			"2": types.MustSignNewTx(prviKey2, signer, &types.LegacyTx{Gas: 21000, Nonce: 2}),
-			"1": types.MustSignNewTx(prviKey2, signer, &types.LegacyTx{Gas: 21000, Nonce: 1}),
-		},
-		localAddress: map[string]*types.Transaction{
-			"9":  types.MustSignNewTx(localPrivKey, signer, &types.LegacyTx{Gas: 21000, Nonce: 9}),
-			"10": types.MustSignNewTx(localPrivKey, signer, &types.LegacyTx{Gas: 21000, Nonce: 10}),
-		},
-	}
-
-	locals, remotes := poolContent.ToTxsByPriceAndNonce(common.Big1, []common.Address{localAddress})
-
-	for i := 0; i < 2; i++ {
-		require.NotNil(t, locals.Peek())
-		locals.Shift()
-	}
-
-	require.Nil(t, locals.Peek())
-
-	for i := 0; i < 5; i++ {
-		require.NotNil(t, remotes.Peek())
-		remotes.Shift()
-	}
-
-	require.Nil(t, remotes.Peek())
 }
 
 func TestGetGenesisL1Header(t *testing.T) {
