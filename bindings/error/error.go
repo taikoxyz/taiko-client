a4a25391c3018152898d9f112fba1400f2b84dfe
alex <alex@taiko.xyz>
2023-02-21 15:26:42 +0800

    refactor: do some refactor

--- bindings/error/error.go
+++ bindings/error/error.go
@@ -1,12 +1,15 @@
 package error
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// copy form https://github.com/ethereum/go-ethereum/blob/6428663faf50f8368cedf0297063154483cce72b/rpc/json.go#L51
+// JsonRPCError is copied from
+// https://github.com/ethereum/go-ethereum/blob/6428663faf50f8368cedf0297063154483cce72b/rpc/json.go#L51
 type JsonRPCError struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
@@ -28,11 +31,33 @@ func (err *JsonRPCError) ErrorData() interface{} {
 	return err.Data
 }
 
-func CheckProtocolRevertReason(errString string, err *JsonRPCError) bool {
-	reason, ok := err.ErrorData().(string)
-	if !ok {
-		return false
+// GetRevertReasonHash parses solidity contract revert reason hash
+// from ethereum rpc calls.
+func GetRevertReasonHash(err error) (string, error) {
+	bytes, err := json.Marshal(errors.Unwrap(err))
+	if err != nil {
+		return "", err
 	}
-	expect := fmt.Sprintf("%#x", crypto.Keccak256([]byte(errString)))[:10]
-	return expect == reason
+	rErr := new(JsonRPCError)
+	if err = json.Unmarshal(bytes, rErr); err != nil {
+		return "", err
+	}
+	reasonHash, ok := rErr.ErrorData().(string)
+	if !ok {
+		return "", fmt.Errorf("invalid revert error, %T", rErr.ErrorData())
+	}
+	return reasonHash, nil
+}
+
+// CheckExpectRevertReason checks if the revert reason in solidity contracts matches the expectation.
+func CheckExpectRevertReason(expect string, revertErr error) (bool, error) {
+	reason, err := GetRevertReasonHash(revertErr)
+	if err != nil {
+		return false, err
+	}
+	hash := fmt.Sprintf("%#x", crypto.Keccak256([]byte(expect)))[:10]
+	if hash != reason {
+		fmt.Printf("expect=%s,reason=%s", hash, reason)
+	}
+	return hash == reason, nil
 }
