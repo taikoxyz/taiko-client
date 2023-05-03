956f12bbdb1873311e55eff07836636e3d815ef1
Jeffery Walsh <cyberhorsey@gmail.com>
2023-05-02 19:28:51 -0700

    delete

--- prover/prover.go
+++ prover/prover.go
@@ -302,6 +302,7 @@ func (p *Prover) onBlockProposed(
 
 	go func() {
 		if err := handleBlockProposedEvent(); err != nil {
+			delete(p.currentBlocksBeingProven, event.Id.Uint64())
 			log.Error("Handle new BlockProposed event error", "error", err)
 		}
 	}()
