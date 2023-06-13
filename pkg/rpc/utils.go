95cb792bdac0457b122afdfb3a6c41b14902527c
David <david@taiko.xyz>
2023-06-13 17:08:42 +0800

    feat(proposer): add `waitReceiptTimeout` when proposing (#282)

--- pkg/rpc/utils.go
+++ pkg/rpc/utils.go
@@ -18,6 +18,11 @@ import (
 	"github.com/taikoxyz/taiko-client/bindings/encoding"
 )
 
+var (
+	waitReceiptPollingInterval = 3 * time.Second
+	defaultWaitReceiptTimeout  = 1 * time.Minute
+)
+
 // GetProtocolStateVariables gets the protocol states from TaikoL1 contract.
 func GetProtocolStateVariables(
 	taikoL1Client *bindings.TaikoL1Client,
@@ -30,41 +35,24 @@ func GetProtocolStateVariables(
 	return &stateVars, nil
 }
 
-// WaitConfirmations won't return before N blocks confirmations have been seen
-// on destination chain.
-func WaitConfirmations(ctx context.Context, client *ethclient.Client, confirmations uint64, begin uint64) error {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-ticker.C:
-			latest, err := client.BlockNumber(ctx)
-			if err != nil {
-				log.Error("Fetch latest block number error: %w", err)
-				continue
-			}
-
-			if latest < begin+confirmations {
-				continue
-			}
-
-			return nil
-		}
-	}
-}
-
 // WaitReceipt keeps waiting until the given transaction has an execution
 // receipt to know whether it was reverted or not.
 func WaitReceipt(ctx context.Context, client *ethclient.Client, tx *types.Transaction) (*types.Receipt, error) {
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(waitReceiptPollingInterval)
 	defer ticker.Stop()
+
+	var (
+		ctxWithTimeout = ctx
+		cancel         context.CancelFunc
+	)
+	if _, ok := ctx.Deadline(); !ok {
+		ctxWithTimeout, cancel = context.WithTimeout(ctx, defaultWaitReceiptTimeout)
+		defer cancel()
+	}
 
 	for {
 		select {
-		case <-ctx.Done():
+		case <-ctxWithTimeout.Done():
 			return nil, ctx.Err()
 		case <-ticker.C:
 			receipt, err := client.TransactionReceipt(ctx, tx.Hash())
@@ -79,36 +67,6 @@ func WaitReceipt(ctx context.Context, client *ethclient.Client, tx *types.Transa
 			return receipt, nil
 		}
 	}
-}
-
-// GetReceiptsByBlock fetches all transaction receipts in a block.
-func GetReceiptsByBlock(ctx context.Context, cli *rpc.Client, block *types.Block) (types.Receipts, error) {
-	reqs := make([]rpc.BatchElem, block.Transactions().Len())
-	receipts := make([]*types.Receipt, block.Transactions().Len())
-
-	for i, tx := range block.Transactions() {
-		reqs[i] = rpc.BatchElem{
-			Method: "eth_getTransactionReceipt",
-			Args:   []interface{}{tx.Hash()},
-			Result: &receipts[i],
-		}
-	}
-
-	if err := cli.BatchCallContext(ctx, reqs); err != nil {
-		return nil, err
-	}
-
-	for i := range reqs {
-		if reqs[i].Error != nil {
-			return nil, reqs[i].Error
-		}
-
-		if receipts[i] == nil {
-			return nil, fmt.Errorf("got null receipt of transaction %s", block.Transactions()[i].Hash())
-		}
-	}
-
-	return receipts, nil
 }
 
 // NeedNewSystemProof checks whether the L2 block still needs a new system proof.
