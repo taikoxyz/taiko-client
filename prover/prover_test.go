2f47261eb986d693c9da3d3e8d130fca1d0c3b47
Jeffery Walsh <cyberhorsey@gmail.com>
2023-07-01 02:05:23 -0700

    test

--- prover/prover_test.go
+++ prover/prover_test.go
@@ -36,17 +36,18 @@ func (s *ProverTestSuite) SetupTest() {
 	ctx, cancel := context.WithCancel(context.Background())
 	p := new(Prover)
 	s.Nil(InitFromConfig(ctx, p, (&Config{
-		L1WsEndpoint:             os.Getenv("L1_NODE_WS_ENDPOINT"),
-		L1HttpEndpoint:           os.Getenv("L1_NODE_HTTP_ENDPOINT"),
-		L2WsEndpoint:             os.Getenv("L2_EXECUTION_ENGINE_WS_ENDPOINT"),
-		L2HttpEndpoint:           os.Getenv("L2_EXECUTION_ENGINE_HTTP_ENDPOINT"),
-		TaikoL1Address:           common.HexToAddress(os.Getenv("TAIKO_L1_ADDRESS")),
-		TaikoL2Address:           common.HexToAddress(os.Getenv("TAIKO_L2_ADDRESS")),
-		TaikoProverPoolL1Address: common.HexToAddress(os.Getenv("TAIKO_PROVER_POOL_L1_ADDRESS")),
-		L1ProverPrivKey:          l1ProverPrivKey,
-		OracleProverPrivateKey:   l1ProverPrivKey,
-		Dummy:                    true,
-		MaxConcurrentProvingJobs: 1,
+		L1WsEndpoint:                             os.Getenv("L1_NODE_WS_ENDPOINT"),
+		L1HttpEndpoint:                           os.Getenv("L1_NODE_HTTP_ENDPOINT"),
+		L2WsEndpoint:                             os.Getenv("L2_EXECUTION_ENGINE_WS_ENDPOINT"),
+		L2HttpEndpoint:                           os.Getenv("L2_EXECUTION_ENGINE_HTTP_ENDPOINT"),
+		TaikoL1Address:                           common.HexToAddress(os.Getenv("TAIKO_L1_ADDRESS")),
+		TaikoL2Address:                           common.HexToAddress(os.Getenv("TAIKO_L2_ADDRESS")),
+		TaikoProverPoolL1Address:                 common.HexToAddress(os.Getenv("TAIKO_PROVER_POOL_L1_ADDRESS")),
+		L1ProverPrivKey:                          l1ProverPrivKey,
+		OracleProverPrivateKey:                   l1ProverPrivKey,
+		Dummy:                                    true,
+		MaxConcurrentProvingJobs:                 1,
+		CheckProofWindowExpiredIntervalInSeconds: time.Duration(5 * time.Second),
 	})))
 	s.p = p
 	s.cancel = cancel
