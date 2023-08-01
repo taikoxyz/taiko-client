bcb557de734433cf5cc09c4a96e836326808e475
Jeffery Walsh <cyberhorsey@gmail.com>
2023-08-01 13:24:37 -0700

    tests

--- prover/proof_submitter/valid_proof_submitter_test.go
+++ prover/proof_submitter/valid_proof_submitter_test.go
@@ -83,6 +83,7 @@ func (s *ProofSubmitterTestSuite) SetupTest() {
 		L2SuggestedFeeRecipient:    common.HexToAddress(os.Getenv("L2_SUGGESTED_FEE_RECIPIENT")),
 		ProposeInterval:            &proposeInterval, // No need to periodically propose transactions list in unit tests
 		MaxProposedTxListsPerEpoch: 1,
+		WaitReceiptTimeout:         10 * time.Second,
 	})))
 
 	s.proposer = prop
