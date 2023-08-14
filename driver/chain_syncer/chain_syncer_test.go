e134d511c36c1b5c03d786d5342ebda78fc95875
RogerLamTd <bob.rogerbob1@gmail.com>
2023-08-13 20:57:56 -0400

    chainsyncer tests

--- driver/chain_syncer/chain_syncer_test.go
+++ driver/chain_syncer/chain_syncer_test.go
@@ -2,6 +2,7 @@ package chainSyncer
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -10,14 +11,16 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/suite"
 	"github.com/taikoxyz/taiko-client/driver/state"
+	"github.com/taikoxyz/taiko-client/pkg/rpc"
 	"github.com/taikoxyz/taiko-client/proposer"
 	"github.com/taikoxyz/taiko-client/testutils"
 )
 
 type ChainSyncerTestSuite struct {
 	testutils.ClientTestSuite
-	s *L2ChainSyncer
-	p testutils.Proposer
+	s          *L2ChainSyncer
+	snapshotID string
+	p          testutils.Proposer
 }
 
 func (s *ChainSyncerTestSuite) SetupTest() {
@@ -74,22 +77,54 @@ func (s *ChainSyncerTestSuite) TestSync() {
 // 	s.s.state.setLatestVerifiedBlockHash(common.Hash{})
 // }
 
-// func (s *ChainSyncerTestSuite) TestAheadOfProtocolVerifiedHead2() {
-// 	testutils.ProposeAndInsertEmptyBlocks(&s.ClientTestSuite, s.p, s.s.calldataSyncer)
-// 	head, err := s.RpcClient.L2.HeaderByNumber(context.Background(), nil)
-// 	s.Nil(err)
-// 	// NOTE: verify the block so that the state returns a value > 0
-// 	// s.Nil(s.s.state.VerifyL2Block(context.Background(), head.Number, head.Hash()))
-// 	tx, err := s.s.rpc.TaikoL1.VerifyBlocks(nil, common.Big1)
-// 	s.Nil(err)
-// 	fmt.Printf("tx: %v\n", tx.Hash().Hex())
-// 	fmt.Printf("L1HeaderByNumber head: %v\n", head.Number)
-// 	fmt.Printf("LatestVerifiedBlock number: %v\n", s.s.state.GetLatestVerifiedBlock().ID.Uint64())
-// 	fmt.Printf("LatestL2Head Number: %v\n", s.s.state.GetL2Head().Number)
-// }
+func (s *ChainSyncerTestSuite) TestAheadOfProtocolVerifiedHead2() {
+	s.TakeSnapshot()
+	// propose a couple blocks
+	testutils.ProposeAndInsertEmptyBlocks(&s.ClientTestSuite, s.p, s.s.calldataSyncer)
+
+	var result uint64
+	s.RpcClient.L1RawRPC.CallContext(context.Background(), &result, "evm_increaseTime", 2000)
+	s.NotNil(result)
+	fmt.Printf("evm time increase: %v\n", result)
+
+	head, err := s.RpcClient.L2.HeaderByNumber(context.Background(), nil)
+	s.Nil(err)
+
+	fmt.Printf("L1HeaderByNumber head: %v\n", head.Number)
+	fmt.Printf("LatestVerifiedBlock number: %v\n", s.s.state.GetLatestVerifiedBlock().ID.Uint64())
+	fmt.Printf("LatestL2Head Number: %v\n", s.s.state.GetL2Head().Number)
+
+	// NOTE: verify the block so that the state returns a value > 0
+	// Can't figure out how to do this, all the listed methods below don't work / give
+	// nil pointer referencing errors or just aren't relevant.
+
+	// s.s.rpc.TaikoL1.TaikoL1ClientTransactor.VerifyBlocks(nil, common.Big1)
+	// s.s.rpc.TaikoL1.VerifyBlocks(nil, common.Big1)
+	// s.Nil(s.s.state.VerifyL2Block(context.Background(), head.Number, head.Hash()))
+	// tx, err := s.s.rpc.TaikoL1.VerifyBlocks(nil, common.Big1)
+	// s.Nil(err)
+	// err2 := s.s.rpc.TaikoL1.TaikoL1ClientCaller.contract.Call(nil, &out, "getBlock", common.Big0)
+	// s.Nil(err)
+	// fmt.Printf("tx: %v\n", tx.Hash().Hex())
+
+	s.RevertSnapshot()
+}
 
 func TestChainSyncerTestSuite(t *testing.T) {
 	suite.Run(t, new(ChainSyncerTestSuite))
+}
+
+func (s *ChainSyncerTestSuite) TakeSnapshot() {
+	// record snapshot state to revert to before changes
+	s.Nil(s.RpcClient.L1RawRPC.CallContext(context.Background(), &s.snapshotID, "evm_snapshot"))
+}
+
+func (s *ChainSyncerTestSuite) RevertSnapshot() {
+	// revert to the snapshot state so protocol configs are unaffected
+	var revertRes bool
+	s.Nil(s.RpcClient.L1RawRPC.CallContext(context.Background(), &revertRes, "evm_revert", s.snapshotID))
+	s.True(revertRes)
+	s.Nil(rpc.SetHead(context.Background(), s.RpcClient.L2RawRPC, common.Big0))
 }
 
 func (s *ChainSyncerTestSuite) TestAheadOfProtocolVerifiedHead() {
