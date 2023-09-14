cf35c49a2e964ec35de760b905129d91ed14f63a
alex <alex@taiko.xyz>
2023-09-14 20:46:14 +0800

    feat: use httptest.Server to simplify the prover web server test code

--- prover/server/server_test.go
+++ prover/server/server_test.go
@@ -1,36 +1,30 @@
 package server
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"os"
 	"testing"
 	"time"
 
-	"github.com/cenkalti/backoff/v4"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/go-resty/resty/v2"
 	echo "github.com/labstack/echo/v4"
-	"github.com/phayes/freeport"
 	"github.com/stretchr/testify/suite"
 	capacity "github.com/taikoxyz/taiko-client/prover/capacity_manager"
 )
 
 type ProverServerTestSuite struct {
 	suite.Suite
-	srv *ProverServer
+	ps *ProverServer
+	ws *httptest.Server // web server
 }
 
 func (s *ProverServerTestSuite) SetupTest() {
 	l1ProverPrivKey, err := crypto.ToECDSA(common.Hex2Bytes(os.Getenv("L1_PROVER_PRIVATE_KEY")))
-	s.Nil(err)
+	s.NoError(err)
 
-	srv := &ProverServer{
+	p := &ProverServer{
 		echo:             echo.New(),
 		proverPrivateKey: l1ProverPrivKey,
 		minProofFee:      common.Big1,
@@ -38,64 +32,31 @@ func (s *ProverServerTestSuite) SetupTest() {
 		capacityManager:  capacity.New(1024),
 	}
 
-	srv.echo.HideBanner = true
-	srv.configureMiddleware()
-	srv.configureRoutes()
-
-	s.srv = srv
+	p.echo.HideBanner = true
+	p.configureMiddleware()
+	p.configureRoutes()
+	s.ps = p
+	s.ws = httptest.NewServer(p.echo)
 }
 
 func (s *ProverServerTestSuite) TestHealth() {
-	s.Equal(http.StatusOK, s.sendReq("/healthz").Code)
+	s.Equal(http.StatusOK, s.sendReq("/healthz").StatusCode)
 }
 
 func (s *ProverServerTestSuite) TestRoot() {
-	s.Equal(http.StatusOK, s.sendReq("/").Code)
-}
-
-func (s *ProverServerTestSuite) TestStartShutdown() {
-	port, err := freeport.GetFreePort()
-	s.Nil(err)
-
-	url, err := url.Parse(fmt.Sprintf("http://localhost:%v", port))
-	s.Nil(err)
-
-	go func() {
-		if err := s.srv.Start(fmt.Sprintf(":%v", port)); err != nil {
-			log.Error("Failed to start prover server", "error", err)
-		}
-	}()
-
-	// Wait till the server fully started.
-	s.Nil(backoff.Retry(func() error {
-		res, err := resty.New().R().Get(url.String() + "/healthz")
-		if err != nil {
-			return err
-		}
-		if !res.IsSuccess() {
-			return fmt.Errorf("invalid response status code: %d", res.StatusCode())
-		}
-
-		return nil
-	}, backoff.NewExponentialBackOff()))
-
-	s.Nil(s.srv.Shutdown(context.Background()))
+	s.Equal(http.StatusOK, s.sendReq("/").StatusCode)
 }
 
 func (s *ProverServerTestSuite) TearDownTest() {
-	s.Nil(s.srv.Shutdown(context.Background()))
+	s.ws.Close()
 }
 
 func TestProverServerTestSuite(t *testing.T) {
 	suite.Run(t, new(ProverServerTestSuite))
 }
 
-func (s *ProverServerTestSuite) sendReq(path string) *httptest.ResponseRecorder {
-	req, err := http.NewRequest(echo.GET, path, nil)
-	s.Nil(err)
-	rec := httptest.NewRecorder()
-
-	s.srv.ServeHTTP(rec, req)
-
-	return rec
+func (s *ProverServerTestSuite) sendReq(path string) *http.Response {
+	resp, err := http.Get(s.ws.URL + path)
+	s.NoError(err)
+	return resp
 }
