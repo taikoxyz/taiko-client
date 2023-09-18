9fd2b58dd3ccc86ac3fc3c722e9b232dd8f6d211
RogerLamTd <bob.rogerbob1@gmail.com>
2023-09-18 12:55:44 -0700

    restore start and shutdown test

--- prover/server/server_test.go
+++ prover/server/server_test.go
@@ -1,15 +1,22 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/cenkalti/backoff"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/go-resty/resty/v2"
 	echo "github.com/labstack/echo/v4"
+	"github.com/phayes/freeport"
 	"github.com/stretchr/testify/suite"
 	capacity "github.com/taikoxyz/taiko-client/prover/capacity_manager"
 )
@@ -45,6 +52,35 @@ func (s *ProverServerTestSuite) TestHealth() {
 
 func (s *ProverServerTestSuite) TestRoot() {
 	s.Equal(http.StatusOK, s.sendReq("/").StatusCode)
+}
+
+func (s *ProverServerTestSuite) TestStartShutdown() {
+	port, err := freeport.GetFreePort()
+	s.Nil(err)
+
+	url, err := url.Parse(fmt.Sprintf("http://localhost:%v", port))
+	s.Nil(err)
+
+	go func() {
+		if err := s.ps.Start(fmt.Sprintf(":%v", port)); err != nil {
+			log.Error("Failed to start prover server", "error", err)
+		}
+	}()
+
+	// Wait till the server fully started.
+	s.Nil(backoff.Retry(func() error {
+		res, err := resty.New().R().Get(url.String() + "/healthz")
+		if err != nil {
+			return err
+		}
+		if !res.IsSuccess() {
+			return fmt.Errorf("invalid response status code: %d", res.StatusCode())
+		}
+
+		return nil
+	}, backoff.NewExponentialBackOff()))
+
+	s.Nil(s.ps.Shutdown(context.Background()))
 }
 
 func (s *ProverServerTestSuite) TearDownTest() {
