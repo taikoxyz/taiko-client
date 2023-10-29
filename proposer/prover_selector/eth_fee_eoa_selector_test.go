5d866fb4159371c39032a2647d1a597c48735428
David <david@taiko.xyz>
2023-10-29 17:42:45 +0800

    feat: update tests

--- proposer/prover_selector/eth_fee_eoa_selector_test.go
+++ proposer/prover_selector/eth_fee_eoa_selector_test.go
@@ -1,6 +1,7 @@
 package selector
 
 import (
+	"context"
 	"net/url"
 	"os"
 	"testing"
@@ -45,6 +46,18 @@ func (s *ProverSelectorTestSuite) SetupTest() {
 
 func (s *ProverSelectorTestSuite) TestProverEndpoints() {
 	s.Equal(1, len(s.s.ProverEndpoints()))
+}
+
+func (s *ProverSelectorTestSuite) TestProverAssignProver() {
+	sig, fee, err := s.s.AssignProver(context.Background(), []encoding.TierFee{
+		{Tier: encoding.TierOptimisticID, Fee: common.Big256},
+		{Tier: encoding.TierSgxID, Fee: common.Big256},
+		{Tier: encoding.TierPseZkevmID, Fee: common.Big256},
+		{Tier: encoding.TierSgxAndPseZkevmID, Fee: common.Big256},
+	}, testutils.RandomHash())
+	s.NotEmpty(sig)
+	s.True(fee.Cmp(common.Big0) > 0)
+	s.Nil(err)
 }
 
 func TestProverSelectorTestSuite(t *testing.T) {
