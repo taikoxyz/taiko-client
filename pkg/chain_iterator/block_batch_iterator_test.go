c471943a3162211fb5d1b842defac32533453db0
Jeffery Walsh <cyberhorsey@gmail.com>
2023-11-22 11:43:14 -0800

    use a higher number for block batch iterator tests

--- pkg/chain_iterator/block_batch_iterator_test.go
+++ pkg/chain_iterator/block_batch_iterator_test.go
@@ -227,7 +227,7 @@ func (s *BlockBatchIteratorTestSuite) TestBlockBatchIteratorConfig() {
 			endIterFunc()
 			return nil
 		},
-		StartHeight: common.Big256,
+		StartHeight: big.NewInt(100000), // use very high number
 		EndHeight:   common.Big256,
 	})
 	s.ErrorContains(err6, "failed to get start header")
@@ -246,7 +246,7 @@ func (s *BlockBatchIteratorTestSuite) TestBlockBatchIteratorConfig() {
 			return nil
 		},
 		StartHeight: common.Big0,
-		EndHeight:   common.Big256,
+		EndHeight:   big.NewInt(100000), // use very high number
 	})
 	s.ErrorContains(err7, "failed to get end header")
 }
