48b13ea8bf4bef0a643d26e3f310e82f9401047d
maskpp <maskpp266@gmail.com>
2024-01-11 23:00:19 +0800

    Update docker/start.sh
    Co-authored-by: David <david@taiko.xyz>

--- docker/start.sh
+++ docker/start.sh
@@ -8,7 +8,7 @@ echo "start docker compose service: ${DOCKER_SERVICE_LIST[*]}"
 
 compose_up "${DOCKER_SERVICE_LIST[@]}"
 
-# check until l1 chain is ready
+# check until L1 chain is ready
 until cast chain-id --rpc-url "$L2_EXECUTION_ENGINE_HTTP_ENDPOINT"; do
     sleep 1
 done
