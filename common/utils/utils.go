1939be755c8b617765d13b7b913010b0129641d3
David <david@taiko.xyz>
2024-01-14 10:15:59 +0800

    Update common/utils/utils.go

--- common/utils/utils.go
+++ common/utils/utils.go
@@ -2,7 +2,7 @@ package utils
 
 import "github.com/modern-go/reflect2"
 
-// IsNil Check if the interface is empty.
+// IsNil checks if the interface is empty.
 func IsNil(i interface{}) bool {
 	return i == nil || reflect2.IsNil(i)
 }
