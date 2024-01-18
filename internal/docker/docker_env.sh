20af9747443ef94b82b668a734c314f0f834c8fd
maskpp <maskpp266@gmail.com>
2024-01-18 22:58:03 +0800

    dynamic docker ports

--- internal/docker/docker_env.sh
+++ internal/docker/docker_env.sh
@@ -2,13 +2,19 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export L1_NODE_HTTP_ENDPOINT=http://localhost:18545
-export L1_NODE_WS_ENDPOINT=ws://localhost:18546
-export L1_EXECUTION_ENGINE_AUTH_ENDPOINT=http://localhost:18551
+export L1_NODE_HTTP_ENDPOINT=http://localhost:$(docker port l1_node | awk -F ':' '{print $2}')
+export L1_NODE_WS_ENDPOINT=ws://localhost:$(docker port l1_node | awk -F ':' '{print $2}')
 
-export L2_EXECUTION_ENGINE_HTTP_ENDPOINT=http://localhost:28545
-export L2_EXECUTION_ENGINE_WS_ENDPOINT=ws://localhost:28546
-export L2_EXECUTION_ENGINE_AUTH_ENDPOINT=http://localhost:28551
+export L2_EXECUTION_ENGINE_HTTP_ENDPOINT=http://localhost:$(docker port l2_node | awk -F ':' 'NR==1 {print $2}')
+export L2_EXECUTION_ENGINE_WS_ENDPOINT=ws://localhost:$(docker port l2_node | awk -F ':' 'NR==2 {print $2}')
+export L2_EXECUTION_ENGINE_AUTH_ENDPOINT=http://localhost:$(docker port l2_node | awk -F ':' 'NR==3 {print $2}')
 export JWT_SECRET=$DIR/nodes/jwt.hex
 
 export DOCKER_SERVICE_LIST=("l1_node" "l2_execution_engine")
+
+echo "L1_NODE_HTTP_ENDPOINT: $L1_NODE_HTTP_ENDPOINT"
+echo "L1_NODE_WS_ENDPOINT: $L1_NODE_WS_ENDPOINT"
+
+echo "L2_EXECUTION_ENGINE_HTTP_ENDPOINT: $L2_EXECUTION_ENGINE_HTTP_ENDPOINT"
+echo "L2_EXECUTION_ENGINE_WS_ENDPOINT: $L2_EXECUTION_ENGINE_WS_ENDPOINT"
+echo "L2_EXECUTION_ENGINE_AUTH_ENDPOINT: $L2_EXECUTION_ENGINE_AUTH_ENDPOINT"
--- internal/docker/start.sh
+++ internal/docker/start.sh
@@ -10,6 +10,7 @@ compose_up "${DOCKER_SERVICE_LIST[@]}"
 
 # check until L1 chain is ready
 until cast chain-id --rpc-url "$L2_EXECUTION_ENGINE_HTTP_ENDPOINT"; do
+    source internal/docker/docker_env.sh
     sleep 1
 done
 
