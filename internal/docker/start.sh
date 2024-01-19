6e1de08bff417f86cf2a2e260d35cfe1293e4bfe
maskpp <maskpp266@gmail.com>
2024-01-19 09:58:30 +0800

    hide error message

--- internal/docker/start.sh
+++ internal/docker/start.sh
@@ -9,7 +9,7 @@ echo "start docker compose service: ${DOCKER_SERVICE_LIST[*]}"
 compose_up "${DOCKER_SERVICE_LIST[@]}"
 
 # check until L1 chain is ready
-until cast chain-id --rpc-url "$L2_EXECUTION_ENGINE_HTTP_ENDPOINT"; do
+until cast chain-id --rpc-url "$L2_EXECUTION_ENGINE_HTTP_ENDPOINT" 2> /dev/null; do
     source internal/docker/docker_env.sh
     sleep 1
 done
