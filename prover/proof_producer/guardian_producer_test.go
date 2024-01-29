3fb66e44ae599ef1df693c41002baf235861b641
RogerLamTd <bob.rogerbob1@gmail.com>
2024-01-29 12:38:35 -0800

    add test

--- prover/proof_producer/guardian_producer_test.go
+++ prover/proof_producer/guardian_producer_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 
 	"github.com/taikoxyz/taiko-client/bindings"
@@ -48,4 +49,42 @@ func TestGuardianProducerRequestProof(t *testing.T) {
 	require.Equal(t, res.Header, header)
 	require.Equal(t, res.Tier, encoding.TierGuardianID)
 	require.NotEmpty(t, res.Proof)
+}
+
+func TestGuardianProducerRequestProofReturnLivenessBond(t *testing.T) {
+	header := &types.Header{
+		ParentHash:  randHash(),
+		UncleHash:   randHash(),
+		Coinbase:    common.BytesToAddress(randHash().Bytes()),
+		Root:        randHash(),
+		TxHash:      randHash(),
+		ReceiptHash: randHash(),
+		Difficulty:  common.Big0,
+		Number:      common.Big256,
+		GasLimit:    1024,
+		GasUsed:     1024,
+		Time:        uint64(time.Now().Unix()),
+		Extra:       randHash().Bytes(),
+		MixDigest:   randHash(),
+		Nonce:       types.BlockNonce{},
+	}
+
+	var (
+		producer = &GuardianProofProducer{LivenessBond: true}
+		blockID  = common.Big32
+	)
+	res, err := producer.RequestProof(
+		context.Background(),
+		&ProofRequestOptions{},
+		blockID,
+		&bindings.TaikoDataBlockMetadata{},
+		header,
+	)
+	require.Nil(t, err)
+
+	require.Equal(t, res.BlockID, blockID)
+	require.Equal(t, res.Header, header)
+	require.Equal(t, res.Tier, encoding.TierGuardianID)
+	require.NotEmpty(t, res.Proof)
+	require.Equal(t, res.Proof, crypto.Keccak256([]byte("RETURN_LIVENESS_BOND")))
 }
