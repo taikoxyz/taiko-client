5716c6d3ddf3681fe583b8683ca6449070e627a5
David <david@taiko.xyz>
2024-01-31 21:18:30 +0800

    feat: update workflow

--- bindings/encoding/struct.go
+++ bindings/encoding/struct.go
@@ -17,7 +17,7 @@ var (
 	TierGuardianID       uint16 = 1000
 	ProtocolTiers               = []uint16{TierOptimisticID, TierSgxID, TierSgxAndPseZkevmID, TierGuardianID}
 	AnchorTxGasLimit     uint64 = 250_000
-	GoldenTouchPrivKey          = "0x92954368afd3caa1f3ce3ead0069c1af414054aefe1ef9aeacc1bf426222ce38"
+	GoldenTouchPrivKey          = "92954368afd3caa1f3ce3ead0069c1af414054aefe1ef9aeacc1bf426222ce38"
 )
 
 // BlockHeader represents an Ethereum block header.
