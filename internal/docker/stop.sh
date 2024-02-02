538a9c717b60ed168b70f6a7fbd532bf4d8d5d4c
maskpp <maskpp266@gmail.com>
2024-02-02 12:51:44 +0800

    update docker

--- internal/docker/stop.sh
+++ internal/docker/stop.sh
@@ -2,8 +2,15 @@
 
 source scripts/common.sh
 
-DOCKER_SERVICE_LIST=("l1_node" "l2_execution_engine")
+DOCKER_SERVICE_LIST=("beacon-chain" "geth" "validator" "l2_execution_engine")
 
 echo "stop docker compose service: ${DOCKER_SERVICE_LIST[*]}"
 
 compose_down "${DOCKER_SERVICE_LIST[@]}"
+
+# Delete exited containers.
+docker rm $(docker ps -aqf "status=exited") 2>/dev/null
+
+rm -rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
+rm -rf ./execution/geth
+rm -rf taikogeth/taiko-geth
