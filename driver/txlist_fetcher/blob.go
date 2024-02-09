cca1518269375ee712d9923b3c8856de4789dbaa
David <david@taiko.xyz>
2024-02-09 23:00:36 +0800

    feat: add `kZGToVersionedHash`

--- driver/txlist_fetcher/blob.go
+++ driver/txlist_fetcher/blob.go
@@ -2,13 +2,19 @@ package txlistdecoder
 
 import (
 	"context"
+	"crypto/sha256"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto/kzg4844"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/taikoxyz/taiko-client/bindings"
 	"github.com/taikoxyz/taiko-client/pkg/rpc"
+)
+
+const (
+	blobCommitmentVersionKZG uint8 = 0x01 // Version byte for the point evaluation precompile.
 )
 
 type BlobFetcher struct {
@@ -38,10 +44,20 @@ func (d *BlobFetcher) Fetch(
 	for _, sidecar := range sidecars {
 		log.Info("Found sidecar", "KzgCommitment", sidecar.KzgCommitment, "blobHash", common.Bytes2Hex(meta.BlobHash[:]))
 
-		if sidecar.KzgCommitment == common.Bytes2Hex(meta.BlobHash[:]) {
+		if kZGToVersionedHash(
+			kzg4844.Commitment(common.Hex2Bytes(sidecar.KzgCommitment)[:]),
+		) == common.BytesToHash(meta.BlobHash[:]) {
 			return common.Hex2Bytes(sidecar.Blob), nil
 		}
 	}
 
 	return nil, errSidecarNotFound
+}
+
+// kZGToVersionedHash implements kzg_to_versioned_hash from EIP-4844
+func kZGToVersionedHash(kzg kzg4844.Commitment) common.Hash {
+	h := sha256.Sum256(kzg[:])
+	h[0] = blobCommitmentVersionKZG
+
+	return h
 }
