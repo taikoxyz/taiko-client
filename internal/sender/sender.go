a122b25761ce935adf4baf3d6870617012b41acf
maskpp <maskpp266@gmail.com>
2024-02-21 11:19:09 +0800

    update sender

--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -10,13 +10,11 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	cmap "github.com/orcaman/concurrent-map/v2"
-	"modernc.org/mathutil"
 
 	"github.com/taikoxyz/taiko-client/pkg/rpc"
 )
@@ -25,7 +23,7 @@ type Config struct {
 	// The gap number between a block be confirmed and the latest block.
 	Confirmations uint64
 	// The maximum gas price can be used to send transaction.
-	MaxGasPrice uint64
+	MaxGasPrice *big.Int
 	// The gas rate to increase the gas price.
 	GasRate uint64
 	// The maximum number of pending transactions.
@@ -35,7 +33,7 @@ type Config struct {
 }
 
 type TxConfirm struct {
-	retryTimes uint64
+	RetryTimes uint64
 	confirms   uint64
 
 	TxID string
@@ -51,11 +49,11 @@ type Sender struct {
 	ctx context.Context
 	*Config
 
-	chainID *big.Int
-	header  *types.Header
-	client  *rpc.EthClient
+	header *types.Header
+	client *rpc.EthClient
 
-	opts *bind.TransactOpts
+	ChainID *big.Int
+	Opts    *bind.TransactOpts
 
 	globalTxID     uint64
 	unconfirmedTxs cmap.ConcurrentMap[string, *TxConfirm] //uint64]*TxConfirm
@@ -89,16 +87,16 @@ func NewSender(ctx context.Context, cfg *Config, client *rpc.EthClient, priv *ec
 	sender := &Sender{
 		ctx:            ctx,
 		Config:         cfg,
-		chainID:        chainID,
+		ChainID:        chainID,
 		header:         header,
 		client:         client,
-		opts:           opts,
+		Opts:           opts,
 		unconfirmedTxs: cmap.New[*TxConfirm](),
 		txConfirmCh:    cmap.New[chan *TxConfirm](),
 		stopCh:         make(chan struct{}),
 	}
 	// Set the nonce
-	sender.setNonce()
+	sender.adjustNonce(nil)
 	// Update the gas tip and gas fee.
 	err = sender.updateGasTipGasFee(header)
 	if err != nil {
@@ -123,24 +121,14 @@ func (s *Sender) WaitTxConfirm(txID string) (<-chan *TxConfirm, bool) {
 }
 
 // SendRaw sends a transaction to the target address.
-func (s *Sender) SendRaw(target *common.Address, value *big.Int, data []byte) (string, error) {
-	gasLimit, err := s.client.EstimateGas(s.ctx, ethereum.CallMsg{
-		From:      s.opts.From,
-		To:        target,
-		Value:     value,
-		Data:      data,
-		GasFeeCap: s.opts.GasFeeCap,
-		GasTipCap: s.opts.GasTipCap,
-	})
-	if err != nil {
-		return "", err
-	}
+func (s *Sender) SendRaw(nonce uint64, target *common.Address, value *big.Int, data []byte) (string, error) {
 	return s.SendTransaction(types.NewTx(&types.DynamicFeeTx{
-		ChainID:   s.chainID,
+		ChainID:   s.ChainID,
 		To:        target,
-		GasFeeCap: s.opts.GasFeeCap,
-		GasTipCap: s.opts.GasTipCap,
-		Gas:       gasLimit,
+		Nonce:     nonce,
+		GasFeeCap: s.Opts.GasFeeCap,
+		GasTipCap: s.Opts.GasTipCap,
+		Gas:       1000000,
 		Value:     value,
 		Data:      data,
 	}))
@@ -155,10 +143,11 @@ func (s *Sender) SendTransaction(tx *types.Transaction) (string, error) {
 	confirmTx := &TxConfirm{
 		TxID: txID,
 		baseTx: &types.DynamicFeeTx{
-			ChainID:   s.chainID,
+			ChainID:   s.ChainID,
 			To:        tx.To(),
-			GasFeeCap: tx.GasFeeCap(),
-			GasTipCap: tx.GasTipCap(),
+			Nonce:     tx.Nonce(),
+			GasFeeCap: s.Opts.GasFeeCap,
+			GasTipCap: s.Opts.GasTipCap,
 			Gas:       tx.Gas(),
 			Value:     tx.Value(),
 			Data:      tx.Data(),
@@ -166,7 +155,7 @@ func (s *Sender) SendTransaction(tx *types.Transaction) (string, error) {
 		Tx: tx,
 	}
 	err := s.sendTx(confirmTx)
-	if err != nil {
+	if err != nil && !strings.Contains(err.Error(), "replacement transaction") {
 		log.Error("failed to send transaction", "tx_id", txID, "tx_hash", tx.Hash().String(), "err", err)
 		return "", err
 	}
@@ -181,69 +170,61 @@ func (s *Sender) sendTx(confirmTx *TxConfirm) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	confirmTx.retryTimes++
-	tx := confirmTx.baseTx
-	baseTx := &types.DynamicFeeTx{
-		ChainID:   s.chainID,
-		To:        tx.To,
-		GasFeeCap: tx.GasFeeCap,
-		GasTipCap: tx.GasTipCap,
-		Gas:       tx.Gas,
-		Value:     tx.Value,
-		Data:      tx.Data,
-	}
-	// Try 3 RetryTimes if nonce is not correct.
-	for i := 0; i < 3; i++ {
-		baseTx.Nonce = s.opts.Nonce.Uint64()
-		rawTx, err := s.opts.Signer(s.opts.From, types.NewTx(baseTx))
-		if err != nil {
-			return err
-		}
-		err = s.client.SendTransaction(s.ctx, rawTx)
-		confirmTx.Error = err
-		// Check if the error is nonce too low or nonce too high
-		if err != nil {
-			if strings.Contains(err.Error(), "nonce too low") ||
-				strings.Contains(err.Error(), "nonce too high") {
-				s.setNonce()
-				log.Warn("nonce is not correct, retry to send transaction", "tx_hash", rawTx.Hash().String(), "err", err)
-				time.Sleep(time.Millisecond * 500)
-				continue
-			}
-			log.Error("failed to send transaction", "tx_hash", rawTx.Hash().String(), "err", err)
-			return err
-		}
-		s.opts.Nonce = big.NewInt(s.opts.Nonce.Int64() + 1)
+	baseTx := confirmTx.baseTx
 
-		confirmTx.baseTx = baseTx
-		confirmTx.Tx = rawTx
-		break
+	// Try 3 RetryTimes if nonce is not correct.
+	rawTx, err := s.Opts.Signer(s.Opts.From, types.NewTx(baseTx))
+	if err != nil {
+		return err
 	}
+	confirmTx.Tx = rawTx
+	err = s.client.SendTransaction(s.ctx, rawTx)
+	confirmTx.Error = err
+	// Check if the error is nonce too low.
+	if err != nil {
+		if strings.Contains(err.Error(), "nonce too low") {
+			s.adjustNonce(baseTx)
+			log.Warn("nonce is not correct, retry to send transaction", "tx_hash", rawTx.Hash().String(), "err", err)
+			return nil
+		}
+		if err.Error() == "replacement transaction underpriced" {
+			s.adjustGas(baseTx)
+			log.Warn("replacement transaction underpriced", "tx_hash", rawTx.Hash().String(), "err", err)
+			return nil
+		}
+		log.Error("failed to send transaction", "tx_hash", rawTx.Hash().String(), "err", err)
+		return err
+	}
+	s.Opts.Nonce = big.NewInt(s.Opts.Nonce.Int64() + 1)
 
 	return nil
 }
 
-func (s *Sender) resendTx(confirmTx *TxConfirm) {
-	if confirmTx.retryTimes >= s.RetryTimes {
-		s.unconfirmedTxs.Remove(confirmTx.TxID)
-		s.txConfirmCh.Remove(confirmTx.TxID)
-		return
+func (s *Sender) adjustGas(baseTx *types.DynamicFeeTx) {
+	rate := big.NewInt(int64(100 + s.GasRate))
+	baseTx.GasFeeCap = new(big.Int).Mul(baseTx.GasFeeCap, rate)
+	baseTx.GasFeeCap.Div(baseTx.GasFeeCap, big.NewInt(100))
+	if s.MaxGasPrice.Cmp(baseTx.GasFeeCap) < 0 {
+		baseTx.GasFeeCap = new(big.Int).Set(s.MaxGasPrice)
 	}
 
-	// Increase the gas price.
-	gas := confirmTx.baseTx.Gas
-	confirmTx.baseTx.Gas = mathutil.MinUint64(s.MaxGasPrice, gas+gas/s.GasRate)
-
-	confirmTx.Error = s.sendTx(confirmTx)
+	baseTx.GasTipCap = new(big.Int).Mul(baseTx.GasTipCap, rate)
+	baseTx.GasTipCap.Div(baseTx.GasTipCap, big.NewInt(100))
+	if baseTx.GasTipCap.Cmp(baseTx.GasFeeCap) > 0 {
+		baseTx.GasTipCap = new(big.Int).Set(baseTx.GasFeeCap)
+	}
 }
 
-func (s *Sender) setNonce() {
-	nonce, err := s.client.PendingNonceAt(s.ctx, s.opts.From)
+func (s *Sender) adjustNonce(baseTx *types.DynamicFeeTx) {
+	nonce, err := s.client.NonceAt(s.ctx, s.Opts.From, nil)
 	if err != nil {
-		log.Warn("failed to get the nonce", "from", s.opts.From, "err", err)
+		log.Warn("failed to get the nonce", "from", s.Opts.From, "err", err)
 		return
 	}
-	s.opts.Nonce = new(big.Int).SetUint64(nonce)
+	s.Opts.Nonce = new(big.Int).SetUint64(nonce)
+	if baseTx != nil {
+		baseTx.Nonce = nonce
+	}
 }
 
 func (s *Sender) updateGasTipGasFee(head *types.Header) error {
@@ -259,8 +240,13 @@ func (s *Sender) updateGasTipGasFee(head *types.Header) error {
 	if gasFeeCap.Cmp(gasTipCap) < 0 {
 		return fmt.Errorf("maxFeePerGas (%v) < maxPriorityFeePerGas (%v)", gasFeeCap, gasTipCap)
 	}
-	s.opts.GasTipCap = gasTipCap
-	s.opts.GasFeeCap = gasFeeCap
+	if gasFeeCap.Cmp(s.MaxGasPrice) > 0 {
+		gasFeeCap = new(big.Int).Set(s.MaxGasPrice)
+		gasTipCap = new(big.Int).Set(s.MaxGasPrice)
+	}
+
+	s.Opts.GasTipCap = gasTipCap
+	s.Opts.GasFeeCap = gasFeeCap
 
 	return nil
 }
@@ -268,15 +254,22 @@ func (s *Sender) updateGasTipGasFee(head *types.Header) error {
 func (s *Sender) loop() {
 	defer s.wg.Done()
 
-	tick := time.NewTicker(time.Second * 3)
-	defer tick.Stop()
+	tickHead := time.NewTicker(time.Second * 3)
+	defer tickHead.Stop()
+
+	tickResend := time.NewTicker(time.Second * 2)
+	defer tickResend.Stop()
 
 	for {
 		select {
-		case <-tick.C:
+		case <-tickResend.C:
 			if s.unconfirmedTxs.Count() == 0 {
 				continue
 			}
+			s.resendTransaction()
+			// Check the unconfirmed transactions
+			s.checkPendingTransactions()
+		case <-tickHead.C:
 			head, err := s.client.HeaderByNumber(s.ctx, nil)
 			if err != nil {
 				log.Warn("failed to get the latest header", "err", err)
@@ -292,9 +285,6 @@ func (s *Sender) loop() {
 			if err != nil {
 				log.Warn("failed to update gas tip and gas fee", "err", err)
 			}
-			// Check the unconfirmed transactions
-			s.checkPendingTransactions()
-
 		case <-s.ctx.Done():
 			return
 		case <-s.stopCh:
@@ -303,16 +293,39 @@ func (s *Sender) loop() {
 	}
 }
 
+func (s *Sender) resendTransaction() {
+	for txID, txConfirm := range s.unconfirmedTxs.Items() {
+		if txConfirm.Error == nil {
+			continue
+		}
+		txConfirm.RetryTimes++
+		if s.RetryTimes != 0 && txConfirm.RetryTimes >= s.RetryTimes {
+			s.unconfirmedTxs.Remove(txID)
+			s.txConfirmCh.Remove(txID)
+			continue
+		}
+		_ = s.sendTx(txConfirm)
+	}
+}
+
 func (s *Sender) checkPendingTransactions() {
 	for txID, txConfirm := range s.unconfirmedTxs.Items() {
 		if txConfirm.Error != nil {
-			s.resendTx(txConfirm)
 			continue
 		}
 		if txConfirm.Receipt == nil {
-			// Get the transaction receipt
+			// Ignore the transaction if it is pending.
+			_, isPending, err := s.client.TransactionByHash(s.ctx, txConfirm.Tx.Hash())
+			if err != nil || isPending {
+				continue
+			}
+			// Get the transaction receipt.
 			receipt, err := s.client.TransactionReceipt(s.ctx, txConfirm.Tx.Hash())
 			if err != nil {
+				if err.Error() == "not found" {
+					txConfirm.Error = err
+					s.releaseConfirmCh(txID)
+				}
 				log.Warn("failed to get the transaction receipt", "tx_hash", txConfirm.Tx.Hash().String(), "err", err)
 				continue
 			}
@@ -322,14 +335,19 @@ func (s *Sender) checkPendingTransactions() {
 		txConfirm.confirms = s.header.Number.Uint64() - txConfirm.Receipt.BlockNumber.Uint64()
 		// Check if the transaction is confirmed
 		if s.header.Number.Uint64()-txConfirm.confirms >= s.Confirmations {
-			confirmCh, _ := s.txConfirmCh.Get(txID)
-			select {
-			case confirmCh <- txConfirm:
-			default:
-			}
-			// Remove the transaction from the unconfirmed transactions
-			s.unconfirmedTxs.Remove(txID)
-			s.txConfirmCh.Remove(txID)
+			s.releaseConfirmCh(txID)
 		}
 	}
+}
+
+func (s *Sender) releaseConfirmCh(txID string) {
+	txConfirm, _ := s.unconfirmedTxs.Get(txID)
+	confirmCh, _ := s.txConfirmCh.Get(txID)
+	select {
+	case confirmCh <- txConfirm:
+	default:
+	}
+	// Remove the transaction from the unconfirmed transactions
+	s.unconfirmedTxs.Remove(txID)
+	s.txConfirmCh.Remove(txID)
 }
--- internal/sender/sender_test.go
+++ internal/sender/sender_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/sync/errgroup"
@@ -18,29 +19,38 @@ import (
 	"github.com/taikoxyz/taiko-client/pkg/rpc"
 )
 
-func TestSender(t *testing.T) {
-	utils.LoadEnv()
-
+func setSender(cfg *sender.Config) (*rpc.EthClient, *sender.Sender, error) {
 	ctx := context.Background()
 
 	client, err := rpc.NewEthClient(ctx, os.Getenv("L1_NODE_WS_ENDPOINT"), time.Second*10)
-	assert.NoError(t, err)
+	if err != nil {
+		return nil, nil, err
+	}
 
 	priv, err := crypto.ToECDSA(common.FromHex(os.Getenv("L1_PROPOSER_PRIVATE_KEY")))
-	assert.NoError(t, err)
+	if err != nil {
+		return nil, nil, err
+	}
 
-	send, err := sender.NewSender(ctx, &sender.Config{
-		Confirmations: 1,
-		MaxGasPrice:   1000000000000,
-		GasRate:       10,
+	send, err := sender.NewSender(ctx, cfg, client, priv)
+
+	return client, send, err
+}
+
+func TestNormalSender(t *testing.T) {
+	utils.LoadEnv()
+	_, send, err := setSender(&sender.Config{
+		Confirmations: 0,
+		MaxGasPrice:   big.NewInt(20000000000),
+		GasRate:       50,
 		MaxPendTxs:    10,
-		RetryTimes:    3,
-	}, client, priv)
+		RetryTimes:    0,
+	})
 	assert.NoError(t, err)
 	defer send.Stop()
 
 	var (
-		batchSize  = 10
+		batchSize  = 5
 		eg         errgroup.Group
 		confirmsCh = make([]<-chan *sender.TxConfirm, 0, batchSize)
 	)
@@ -49,7 +59,7 @@ func TestSender(t *testing.T) {
 		i := i
 		eg.Go(func() error {
 			addr := common.BigToAddress(big.NewInt(int64(i)))
-			txID, err := send.SendRaw(&addr, big.NewInt(1), nil)
+			txID, err := send.SendRaw(send.Opts.Nonce.Uint64(), &addr, big.NewInt(1), nil)
 			if err == nil {
 				confirmCh, _ := send.WaitTxConfirm(txID)
 				confirmsCh = append(confirmsCh, confirmCh)
@@ -60,13 +70,111 @@ func TestSender(t *testing.T) {
 	err = eg.Wait()
 	assert.NoError(t, err)
 
-	for len(confirmsCh) > 0 {
-		confirmCh := confirmsCh[0]
-		select {
-		case confirm := <-confirmCh:
-			assert.NoError(t, confirm.Error)
-			confirmsCh = confirmsCh[1:]
-		default:
-		}
+	for ; len(confirmsCh) > 0; confirmsCh = confirmsCh[1:] {
+		confirm := <-confirmsCh[0]
+		assert.NoError(t, confirm.Error)
 	}
+}
+
+// Test touch max gas price and replacement.
+func TestReplacement(t *testing.T) {
+	utils.LoadEnv()
+
+	client, send, err := setSender(&sender.Config{
+		Confirmations: 0,
+		MaxGasPrice:   big.NewInt(20000000000),
+		GasRate:       50,
+		MaxPendTxs:    10,
+		RetryTimes:    0,
+	})
+	assert.NoError(t, err)
+	defer send.Stop()
+
+	// Let max gas price be 2 times of the gas fee cap.
+	send.MaxGasPrice = new(big.Int).Mul(send.Opts.GasFeeCap, big.NewInt(2))
+
+	nonce, err := client.NonceAt(context.Background(), send.Opts.From, nil)
+	assert.NoError(t, err)
+
+	pendingNonce, err := client.PendingNonceAt(context.Background(), send.Opts.From)
+	assert.NoError(t, err)
+	// Run test only if mempool has no pending transactions.
+	if pendingNonce > nonce {
+		return
+	}
+
+	nonce++
+	baseTx := &types.DynamicFeeTx{
+		ChainID:   send.ChainID,
+		To:        &common.Address{},
+		GasFeeCap: send.MaxGasPrice,
+		GasTipCap: send.MaxGasPrice,
+		Nonce:     nonce,
+		Gas:       21000,
+		Value:     big.NewInt(1),
+		Data:      nil,
+	}
+	rawTx, err := send.Opts.Signer(send.Opts.From, types.NewTx(baseTx))
+	assert.NoError(t, err)
+	err = client.SendTransaction(context.Background(), rawTx)
+	assert.NoError(t, err)
+
+	confirmsCh := make([]<-chan *sender.TxConfirm, 0, 5)
+
+	// Replace the transaction with a higher nonce.
+	txID, err := send.SendRaw(nonce, &common.Address{}, big.NewInt(1), nil)
+	assert.NoError(t, err)
+	confirmCh, _ := send.WaitTxConfirm(txID)
+	confirmsCh = append(confirmsCh, confirmCh)
+
+	time.Sleep(time.Second * 6)
+	// Send a transaction with a next nonce and let all the transactions be confirmed.
+	txID, err = send.SendRaw(nonce-1, &common.Address{}, big.NewInt(1), nil)
+	assert.NoError(t, err)
+	confirmCh, _ = send.WaitTxConfirm(txID)
+	confirmsCh = append(confirmsCh, confirmCh)
+
+	for ; len(confirmsCh) > 0; confirmsCh = confirmsCh[1:] {
+		confirm := <-confirmsCh[0]
+		// Check the replaced transaction's gasFeeTap touch the max gas price.
+		if confirm.Tx.Nonce() == nonce {
+			assert.Equal(t, send.MaxGasPrice, confirm.Tx.GasFeeCap())
+		}
+		assert.NoError(t, confirm.Error)
+		t.Log(confirm.Receipt.BlockNumber.String())
+	}
+
+	_, err = client.TransactionReceipt(context.Background(), rawTx.Hash())
+	assert.Equal(t, "not found", err.Error())
+}
+
+// Test nonce too low.
+func TestNonceTooLow(t *testing.T) {
+	utils.LoadEnv()
+
+	client, send, err := setSender(&sender.Config{
+		Confirmations: 0,
+		MaxGasPrice:   big.NewInt(20000000000),
+		GasRate:       50,
+		MaxPendTxs:    10,
+		RetryTimes:    0,
+	})
+	assert.NoError(t, err)
+	defer send.Stop()
+
+	nonce, err := client.NonceAt(context.Background(), send.Opts.From, nil)
+	assert.NoError(t, err)
+	pendingNonce, err := client.PendingNonceAt(context.Background(), send.Opts.From)
+	assert.NoError(t, err)
+	// Run test only if mempool has no pending transactions.
+	if pendingNonce > nonce {
+		return
+	}
+
+	txID, err := send.SendRaw(nonce-3, &common.Address{}, big.NewInt(1), nil)
+	assert.NoError(t, err)
+	confirmCh, _ := send.WaitTxConfirm(txID)
+	confirm := <-confirmCh
+	assert.NoError(t, confirm.Error)
+	assert.Equal(t, nonce, confirm.Tx.Nonce())
 }
