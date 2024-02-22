33ec6032a8e933b8027e1c8e6e3bf721fda2c4f5
maskpp <maskpp266@gmail.com>
2024-02-22 16:27:34 +0800

    upgrade sender test

--- internal/sender/sender_test.go
+++ internal/sender/sender_test.go
@@ -2,6 +2,7 @@ package sender_test
 
 import (
 	"context"
+	"github.com/taikoxyz/taiko-client/internal/utils"
 	"math/big"
 	"os"
 	"runtime"
@@ -11,44 +12,20 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/taikoxyz/taiko-client/internal/sender"
-	"github.com/taikoxyz/taiko-client/internal/utils"
 	"github.com/taikoxyz/taiko-client/pkg/rpc"
 )
 
-func setSender(cfg *sender.Config) (*rpc.EthClient, *sender.Sender, error) {
-	ctx := context.Background()
-
-	client, err := rpc.NewEthClient(ctx, os.Getenv("L1_NODE_WS_ENDPOINT"), time.Second*10)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	priv, err := crypto.ToECDSA(common.FromHex(os.Getenv("L1_PROPOSER_PRIVATE_KEY")))
-	if err != nil {
-		return nil, nil, err
-	}
-
-	send, err := sender.NewSender(ctx, cfg, client, priv)
-
-	return client, send, err
+type SenderTestSuite struct {
+	suite.Suite
+	client *rpc.EthClient
+	sender *sender.Sender
 }
 
-func TestNormalSender(t *testing.T) {
-	utils.LoadEnv()
-	_, send, err := setSender(&sender.Config{
-		MaxGasFee:      20000000000,
-		GasGrowthRate:  50,
-		RetryTimes:     0,
-		GasLimit:       2000000,
-		MaxWaitingTime: time.Second * 10,
-	})
-	assert.NoError(t, err)
-	defer send.Close()
-
+func (s *SenderTestSuite) TestNormalSender() {
 	var (
 		batchSize  = 5
 		eg         errgroup.Group
@@ -59,45 +36,35 @@ func TestNormalSender(t *testing.T) {
 		i := i
 		eg.Go(func() error {
 			addr := common.BigToAddress(big.NewInt(int64(i)))
-			txID, err := send.SendRaw(send.Opts.Nonce.Uint64(), &addr, big.NewInt(1), nil)
+			txID, err := s.sender.SendRaw(s.sender.Opts.Nonce.Uint64(), &addr, big.NewInt(1), nil)
 			if err == nil {
-				confirmCh, _ := send.WaitTxConfirm(txID)
+				confirmCh, _ := s.sender.WaitTxConfirm(txID)
 				confirmsCh = append(confirmsCh, confirmCh)
 			}
 			return err
 		})
 	}
-	err = eg.Wait()
-	assert.NoError(t, err)
+	s.Nil(eg.Wait())
 
 	for ; len(confirmsCh) > 0; confirmsCh = confirmsCh[1:] {
 		confirm := <-confirmsCh[0]
-		assert.NoError(t, confirm.Err)
+		s.Nil(confirm.Err)
 	}
 }
 
 // Test touch max gas price and replacement.
-func TestReplacement(t *testing.T) {
-	utils.LoadEnv()
-
-	client, send, err := setSender(&sender.Config{
-		MaxGasFee:      20000000000,
-		GasGrowthRate:  50,
-		RetryTimes:     0,
-		GasLimit:       2000000,
-		MaxWaitingTime: time.Second * 10,
-	})
-	assert.NoError(t, err)
-	defer send.Close()
+func (s *SenderTestSuite) TestReplacement() {
+	send := s.sender
+	client := s.client
 
 	// Let max gas price be 2 times of the gas fee cap.
 	send.MaxGasFee = send.Opts.GasFeeCap.Uint64() * 2
 
-	nonce, err := client.NonceAt(context.Background(), send.Opts.From, nil)
-	assert.NoError(t, err)
+	nonce, err := s.client.NonceAt(context.Background(), send.Opts.From, nil)
+	s.Nil(err)
 
 	pendingNonce, err := client.PendingNonceAt(context.Background(), send.Opts.From)
-	assert.NoError(t, err)
+	s.Nil(err)
 	// Run test only if mempool has no pending transactions.
 	if pendingNonce > nonce {
 		return
@@ -115,22 +82,22 @@ func TestReplacement(t *testing.T) {
 		Data:      nil,
 	}
 	rawTx, err := send.Opts.Signer(send.Opts.From, types.NewTx(baseTx))
-	assert.NoError(t, err)
+	s.Nil(err)
 	err = client.SendTransaction(context.Background(), rawTx)
-	assert.NoError(t, err)
+	s.Nil(err)
 
 	confirmsCh := make([]<-chan *sender.TxConfirm, 0, 5)
 
 	// Replace the transaction with a higher nonce.
 	txID, err := send.SendRaw(nonce, &common.Address{}, big.NewInt(1), nil)
-	assert.NoError(t, err)
+	s.Nil(err)
 	confirmCh, _ := send.WaitTxConfirm(txID)
 	confirmsCh = append(confirmsCh, confirmCh)
 
 	time.Sleep(time.Second * 6)
 	// Send a transaction with a next nonce and let all the transactions be confirmed.
 	txID, err = send.SendRaw(nonce-1, &common.Address{}, big.NewInt(1), nil)
-	assert.NoError(t, err)
+	s.Nil(err)
 	confirmCh, _ = send.WaitTxConfirm(txID)
 	confirmsCh = append(confirmsCh, confirmCh)
 
@@ -138,42 +105,63 @@ func TestReplacement(t *testing.T) {
 		confirm := <-confirmsCh[0]
 		// Check the replaced transaction's gasFeeTap touch the max gas price.
 		if confirm.Tx.Nonce() == nonce {
-			assert.Equal(t, send.MaxGasFee, confirm.Tx.GasFeeCap().Uint64())
+			s.Equal(send.MaxGasFee, confirm.Tx.GasFeeCap().Uint64())
 		}
-		assert.NoError(t, confirm.Err)
+		s.Nil(confirm.Err)
 	}
 
 	_, err = client.TransactionReceipt(context.Background(), rawTx.Hash())
-	assert.Equal(t, "not found", err.Error())
+	s.Equal("not found", err.Error())
 }
 
 // Test nonce too low.
-func TestNonceTooLow(t *testing.T) {
-	utils.LoadEnv()
-
-	client, send, err := setSender(&sender.Config{
-		MaxGasFee:      20000000000,
-		GasGrowthRate:  50,
-		RetryTimes:     0,
-		GasLimit:       2000000,
-		MaxWaitingTime: time.Second * 10,
-	})
-	assert.NoError(t, err)
-	defer send.Close()
+func (s *SenderTestSuite) TestNonceTooLow() {
+	client := s.client
+	send := s.sender
 
 	nonce, err := client.NonceAt(context.Background(), send.Opts.From, nil)
-	assert.NoError(t, err)
+	s.Nil(err)
 	pendingNonce, err := client.PendingNonceAt(context.Background(), send.Opts.From)
-	assert.NoError(t, err)
+	s.Nil(err)
 	// Run test only if mempool has no pending transactions.
 	if pendingNonce > nonce {
 		return
 	}
 
 	txID, err := send.SendRaw(nonce-3, &common.Address{}, big.NewInt(1), nil)
-	assert.NoError(t, err)
+	s.Nil(err)
 	confirmCh, _ := send.WaitTxConfirm(txID)
 	confirm := <-confirmCh
-	assert.NoError(t, confirm.Err)
-	assert.Equal(t, nonce, confirm.Tx.Nonce())
+	s.Nil(confirm.Err)
+	s.Equal(nonce, confirm.Tx.Nonce())
+}
+
+func (s *SenderTestSuite) SetupTest() {
+	utils.LoadEnv()
+
+	ctx := context.Background()
+	var err error
+	s.client, err = rpc.NewEthClient(ctx, os.Getenv("L1_NODE_WS_ENDPOINT"), time.Second*10)
+	s.Nil(err)
+
+	priv, err := crypto.ToECDSA(common.FromHex(os.Getenv("L1_PROPOSER_PRIVATE_KEY")))
+	s.Nil(err)
+
+	s.sender, err = sender.NewSender(ctx, &sender.Config{
+		MaxGasFee:      20000000000,
+		GasGrowthRate:  50,
+		RetryTimes:     0,
+		GasLimit:       2000000,
+		MaxWaitingTime: time.Second * 10,
+	}, s.client, priv)
+	s.Nil(err)
+}
+
+func (s *SenderTestSuite) TestStartClose() {
+	s.sender.Close()
+	s.client.Close()
+}
+
+func TestSenderTestSuite(t *testing.T) {
+	suite.Run(t, new(SenderTestSuite))
 }
