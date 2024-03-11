a6eda4bc3cf5a30a776563f72e80aceb6ef5de30
maskpp <maskpp266@gmail.com>
2024-03-11 16:08:32 +0800

    fix bug when blob decode with 0

--- pkg/rpc/tx_blob_test.go
+++ pkg/rpc/tx_blob_test.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 	"testing"
 	"time"
@@ -10,6 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/taikoxyz/taiko-client/internal/utils"
@@ -68,4 +70,22 @@ func TestMakeSideCar(t *testing.T) {
 	origin1, dErr := DecodeBlobs(sideCar.Blobs)
 	assert.NoError(t, dErr)
 	assert.Equal(t, origin, origin1)
+}
+
+func TestSpecialEndWith0(t *testing.T) {
+	var txsData = `[{"type":"0x2","chainId":"0x28c59","nonce":"0x1cca","to":"0x0167001000000000000000000000000000010099","gas":"0x86b3","gasPrice":null,"maxPriorityFeePerGas":"0x59682f00","maxFeePerGas":"0x59682f02","value":"0x0","input":"0xa9059cbb00000000000000000000000001670010000000000000000000000000000100990000000000000000000000000000000000000000000000000000000000000001","accessList":[],"v":"0x0","r":"0x2d554e149d15575030f271403a3b359cd9d5df8acb47ae7df5845aadc54b1ee2","s":"0x39b7ce8e803c443d8fd33679948fbd0a485d88b6a55812a53d9a03a922142100","yParity":"0x0","hash":"0x27aa02a44ea343a72131fc67734c67d410ab6f65429637fbb17a08f781e77f7e"}]`
+
+	var txs types.Transactions
+	err := json.Unmarshal([]byte(txsData), &txs)
+	assert.NoError(t, err)
+
+	origin, err := rlp.EncodeToBytes(txs)
+	assert.NoError(t, err)
+
+	blobs := EncodeBlobs(origin)
+
+	data, dErr := DecodeBlobs(blobs)
+	assert.NoError(t, dErr)
+
+	assert.Equal(t, crypto.Keccak256Hash(origin), crypto.Keccak256Hash(data))
 }
