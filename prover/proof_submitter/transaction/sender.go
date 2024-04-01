0b1f1ac8536cdae5aa0f6acc9eeb6aa3cd718b29
Gavin Yu <gavin@taiko.xyz>
2024-04-01 20:37:11 +0800

    Update sender.go

--- prover/proof_submitter/transaction/sender.go
+++ prover/proof_submitter/transaction/sender.go
@@ -9,8 +9,8 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
+	
 	"github.com/taikoxyz/taiko-client/internal/metrics"
-
 	"github.com/taikoxyz/taiko-client/pkg/rpc"
 	producer "github.com/taikoxyz/taiko-client/prover/proof_producer"
 )
