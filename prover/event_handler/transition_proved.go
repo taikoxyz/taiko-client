c76a412a0d722345b1efab8f7ee66f2cfef2d7ed
David <david@taiko.xyz>
2024-04-02 16:40:52 +0800

    fix(prover): fix a check in `isValidProof()`

--- prover/event_handler/transition_proved.go
+++ prover/event_handler/transition_proved.go
@@ -57,6 +57,8 @@ func (h *TransitionProvedEventHandler) Handle(
 		return nil
 	}
 
+	log.Info("Is valid proof", "isValid", isValid)
+
 	// If the proof is invalid, we contest it.
 	blockInfo, err := h.rpc.GetL2BlockInfo(ctx, e.BlockId)
 	if err != nil {
