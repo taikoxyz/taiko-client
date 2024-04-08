8369b864c27e3c90a577f2d292de348531fb7574
maskpp <maskpp266@gmail.com>
2024-04-08 10:19:09 +0800

    fix ci

--- driver/driver.go
+++ driver/driver.go
@@ -65,7 +65,11 @@ func (d *Driver) InitFromConfig(ctx context.Context, cfg *Config) (err error) {
 	if syncMode, err := d.rpc.L2.GetSyncMode(d.ctx); err != nil {
 		return err
 	} else if syncMode != d.SyncMode {
-		return fmt.Errorf("the type is inconsistent with taiko-geth's sync mode, expect: %s, actual: %s", syncMode, d.SyncMode)
+		return fmt.Errorf(
+			"the type is inconsistent with taiko-geth's sync mode, expect: %s, actual: %s",
+			syncMode,
+			d.SyncMode,
+		)
 	}
 
 	if d.state, err = state.New(d.ctx, d.rpc); err != nil {
