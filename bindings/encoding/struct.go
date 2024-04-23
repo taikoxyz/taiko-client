570d8ff26607ebdb99ddf9358c4c4c59119237e0
David <david@taiko.xyz>
2024-04-23 08:18:29 +0800

    feat(prover): improve `getRandomBumpedSubmissionDelay` && bump bindings (#750)

--- bindings/encoding/struct.go
+++ bindings/encoding/struct.go
@@ -10,12 +10,17 @@ import (
 
 // Tier IDs defined in protocol.
 var (
-	TierOptimisticID   uint16 = 100
-	TierSgxID          uint16 = 200
-	TierSgxAndZkVMID   uint16 = 300
-	TierGuardianID     uint16 = 1000
-	ProtocolTiers             = []uint16{TierOptimisticID, TierSgxID, TierSgxAndZkVMID, TierGuardianID}
-	GoldenTouchPrivKey        = "92954368afd3caa1f3ce3ead0069c1af414054aefe1ef9aeacc1bf426222ce38"
+	TierOptimisticID uint16 = 100
+	TierSgxID        uint16 = 200
+	TierSgxAndZkVMID uint16 = 300
+	TierGuardianID   uint16 = 1000
+	ProtocolTiers           = []uint16{
+		TierOptimisticID,
+		TierSgxID,
+		TierSgxAndZkVMID,
+		TierGuardianID,
+	}
+	GoldenTouchPrivKey = "92954368afd3caa1f3ce3ead0069c1af414054aefe1ef9aeacc1bf426222ce38"
 )
 
 // HookCall should be same with TaikoData.HookCall
